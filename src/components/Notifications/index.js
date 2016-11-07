a2248f1fcf3aa27abb7ffabbdfc524b39eb49894
Chris Wright <chris@arcane.ws>
2016-11-06 22:00:16 -0500

    Added Notification component

---
+++ src/components/Notifications/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './style.css';
+
+export default () => (
+    <ul className={styles.container}>
+        <li className={styles.panel}>You have been attacked one of the Overlord's minions!</li>
+        <li className={styles.panel}>CarlCastello has been defeated!</li>
+    </ul>
+);
