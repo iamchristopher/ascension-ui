f8454fb158fa0fe976e6b1e5ee3ac209b0ec8859
Chris Wright <chris@arcane.ws>
2016-11-08 19:46:29 -0500

    Added redux setup

---
+++ src/containers/Chat.js
@@ -0,0 +1,8 @@
+import { connect } from 'react-redux';
+import component from '../components/Chat';
+
+const mapStateToProps = () => ({});
+
+export default connect(
+    mapStateToProps
+)(component);
