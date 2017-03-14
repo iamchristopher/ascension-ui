44feedf692d59952e09456cc266167ca61152dd8
Chris Wright <chris@arcane.ws>
2017-03-14 14:49:08 -0400

    Added server middleware

--- src/stores/configureStore.js
+++ src/stores/configureStore.js
@@ -1,12 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-
+import serverMiddleware from './middleware/server';
 import rootReducer from '../reducers';
 
 export default (initialState) => createStore(
     rootReducer,
     initialState,
     compose(
-        applyMiddleware(thunkMiddleware)
+        applyMiddleware(thunkMiddleware),
+        applyMiddleware(serverMiddleware)
     )
 );
