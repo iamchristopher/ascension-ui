1f5896d9d4dc732cc833817386bd8dbcf2603d51
Chris Wright <chris@arcane.ws>
2017-03-14 15:51:31 -0400

    Added pawn reducer

---
+++ src/reducers/pawn.js
@@ -0,0 +1,27 @@
+const initialState = {};
+
+export default (state = initialState, action = {}) => {
+    const {
+        id,
+        type,
+        ...data
+    } = action;
+
+    switch (type) {
+        case 'PAWN_REGISTER':
+            return {
+                ...state,
+                [id]: data
+            };
+        case 'PAWN_MOVE':
+            return {
+                ...state,
+                [id]: {
+                    ...state.pawns[id],
+                    ...data
+                }
+            };
+        default:
+            return state;
+    }
+};
