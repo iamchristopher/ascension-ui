af540f8e0c76b08edc607ffb6b66005ca0529adf
Chris Wright <chris@arcane.ws>
2017-03-14 14:48:46 -0400

    Added server middleware

---
+++ src/stores/middleware/server.js
@@ -0,0 +1,21 @@
+import {
+    receiveMessage
+} from '../../../actions/chat';
+
+import io from 'socket.io-client/dist/socket.io';
+const connection = io('http://localhost:8080');
+
+export default ({ getState, dispatch }) => {
+    connection.on('MESSAGE', message => dispatch(receiveMessage(message)));
+
+    return next => action => {
+        const {
+            type,
+            value
+        } = action;
+
+        connection.emit(type, value);
+
+        return next(action);
+    };
+};
