103f61dea023121021045f39dc74b91bea547fc0
Chris Wright <chris@arcane.ws>
2017-03-19 22:25:21 -0400

    Changed server middleware now directly dispatches server events

--- src/stores/middleware/server.js
+++ src/stores/middleware/server.js
@@ -1,32 +1,19 @@
 import io from 'socket.io-client/dist/socket.io';
 import wildcard from 'socketio-wildcard';
-import {
-    receiveMessage
-} from '../../actions/chat';
-import * as pawn from '../../actions/pawn';
 
 const connection = io('http://localhost:8080');
 wildcard(io.Manager)(connection);
 
 export default ({ getState, dispatch }) => {
-    connection.on('CHAT_MESSAGE_RECEIVE', message => dispatch(receiveMessage(message)));
-    connection.on('PAWN_MOVE', payload => dispatch(pawn.move({
-        ...payload,
-        sync: false
-    })));
-    connection.on('PAWN_REGISTER', payload => dispatch(pawn.register({
-        ...payload,
-        sync: false
-    })));
-
     connection.on('*', ({
         data: [
             type,
             payload
         ]
-    } = {}) => {
-        // return console.log(type, payload);
-    });
+    } = {}) => dispatch({
+        type,
+        ...payload
+    }));
 
     return next => action => {
         const {
