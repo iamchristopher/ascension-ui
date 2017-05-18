4256cebf20f362b077557620902bd374a0d379bd
Chris Wright <chris@arcane.ws>
2017-05-17 21:14:43 -0400

    Added Tab component

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,3 +1,18 @@
-export const Tab = () => (
-    <div>test</div>
+import React from 'react';
+import styles from './style.css';
+
+export const TabContainer = ({ children }) => (
+    <ul className={styles.tabs}>
+        { children }
+    </ul>
+);
+
+export const Tab = ({
+    isActive,
+    children,
+    onClick = () => {}
+}) => (
+    <li>
+        <a href="#">{ children }</a>
+    </li>
 );
